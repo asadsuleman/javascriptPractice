32b70f4cddcd2cc3e224957d6a741b65c2d582b9
asadsuleman <asadsuleman6363@gmail.com>
2020-10-10 00:33:02 +0500

    Update classJs.js

--- classJs.js
+++ classJs.js
@@ -1,5 +1,5 @@
 //Classes are a template for creating objects. u can create multiple objects after creating a single class
-
+// class is used for making multiple new objects
 //syntax for creating a class
 class car {
     constructor(Model, Engine, Transmission, Color) {
@@ -10,16 +10,28 @@ class car {
         this.Color = Color
 
     }
+    carModel() {
+        console.log(`the car model is ${this.Model}`);
+    }
+
+
+    carColor() {
+        console.log(`the car color is ${this.Color}`);
+    }
 
 }
 
 //syntax for creating a object through class 
 const crolla = new car("GLI", 1600, "auto", 'white'); //here we used 'new ' for new object
-console.log(crolla);
+// console.log(crolla);
 
 const altis = new car("grende", 1800, "auto", "black");
-console.log(altis);
+// console.log(altis);
 
 
-console.log(crolla.Color, altis.Model);
-console.log();
+// console.log(crolla.Color, altis.Model);
+
+altis.carColor();
+altis.carModel();
+
+// crolla.carColor();
